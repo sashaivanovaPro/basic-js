3522206fce1f9776b8d0ee323b58fb32a983ceab
Sasha Ivanova <118177121+sashaivanovaPro@users.noreply.github.com>
2023-12-24 11:34:22 +0600

    Update what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -12,19 +12,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  let month = date.getMonth();
-  if (month < 2 || month == 11 || month === 0) {
-    return "winter";
-  } else if (month < 5) {
-    return "spring";
-  } else if (month < 8) {
-    return "summer";
-  } else if (month < 11) {
-    return "autumn";
+  if (!date) return "Unable to determine the time of year!";
+
+  if (date instanceof Date) {
+    if (Object.prototype.toString.call(date) !== "[object Date]")
+      throw new Error("Invalid date!");
+    const month = date.getMonth();
+    if (month < 2 || month == 11 || month === 0) {
+      return "winter";
+    } else if (month < 5) {
+      return "spring";
+    } else if (month < 8) {
+      return "summer";
+    } else if (month < 11) {
+      return "autumn";
+    }
   }
-  if (!month) {
-    return "Unable to determine the time of year!";
-  }
+  throw new Error("Invalid date!");
 }
 
 module.exports = {
